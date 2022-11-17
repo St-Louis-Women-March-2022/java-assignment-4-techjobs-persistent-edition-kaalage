6c19ab4523a7e64c7ded296d7f1b047ddfa1c181
Karina Lage <karinalage@karinas-air.lan>
2022-11-16 20:36:50 -0600

    Completed Part 1 and Models from Part 2

--- queries.sql
+++ queries.sql
@@ -1,4 +1,8 @@
 -- Part 1: Test it with SQL
+-- INTEGER id;
+-- CHAR(50) employer;
+-- CHAR(50) name;
+-- TINYTEXT skills;
 
 -- Part 2: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 50, message = "Location must have a reasonable length")
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer() {
+    }
 }
