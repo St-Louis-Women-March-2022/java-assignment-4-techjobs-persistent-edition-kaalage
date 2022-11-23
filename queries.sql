3300f3ae290fd8bfbbc4ea85e754a6ff0086d6c1
Karina Lage <karinalage@Karinas-MacBook-Air.local>
2022-11-22 21:42:43 -0600

    Completed Part 2

--- queries.sql
+++ queries.sql
@@ -1,10 +1,13 @@
 -- Part 1: Test it with SQL
--- INTEGER id;
--- CHAR(50) employer;
--- CHAR(50) name;
--- TINYTEXT skills;
+INTEGER id
+CHAR(50) employer
+CHAR(50) name
+TINYTEXT skills;
 
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
